23a769908adc9fc0651c635954d087986656aaa1
MaTheusSlv <matheuscostasilva120899@gmail.com>
2025-12-23 10:35:06 -0300

    Realizado exercício referente a Construtores, palavra This, Sobrecarga e Encapsulamento

---
+++ CursoC-Sharp_Udemy/007ConstrutoresThisSobrecargaEncapsulamento/01_ContaBancaria/01_ContaBancaria/Conta.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace _01_ContaBancaria
+{
+    internal class Conta
+    {
+        public int Numero { get; private set; }
+        public string Nome { get; set; }
+        public double Saldo { get; private set; }
+
+        public Conta(int numero, string nome)
+        {
+            Numero = numero;
+            Nome = nome;
+            Saldo = 0;
+        }
+        public Conta(int numero, string nome, double depositoInicial) : this(numero, nome)
+        {
+            Deposito(depositoInicial);
+        }
+
+        public void Deposito(double quantia)
+        {
+            Saldo = Saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo = Saldo -= quantia + 5.00;
+        }
+
+        public override string ToString()
+        {
+            return "Conta " + Numero + ", Titular: " + Nome + ", Saldo: $ " + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ CursoC-Sharp_Udemy/007ConstrutoresThisSobrecargaEncapsulamento/01_ContaBancaria/01_ContaBancaria/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace _01_ContaBancaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Conta conta;
+
+            Console.Write("Entre com o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Haverá depósito inicial (s/n)? ");
+            char resposta = char.Parse(Console.ReadLine());
+            
+            if (resposta == 's' || resposta == 'S')
+            {
+                Console.Write("Entre com o valor de depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new Conta(numero, nome, depositoInicial);
+            }
+            else
+            {
+                conta = new Conta(numero, nome);
+            }
+
+            Console.WriteLine("\nDados da conta: " + conta);
+
+            Console.Write("\nEntre um valor para depósito: ");
+            double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(deposito);
+            Console.WriteLine("Dados da conta atualizados: " + conta);
+
+            Console.Write("\nEntre um valor para saque: ");
+            double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(saque);
+            Console.WriteLine("Dados da conta atualizados: " + conta);
+        }
+    }
+}
